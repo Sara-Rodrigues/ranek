86e8280cf4777e21036e5f522587db5b3819c365
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-08-02 22:09:23 -0300

    produto filter

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "../views/Home.vue";
+import Produto from "../views/Produto.vue";
 
 Vue.use(Router);
 
@@ -10,8 +11,14 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "Home",
+      name: "home",
       component: Home,
+    },
+    {
+      path: "/produto/:id",
+      name: "produto",
+      component: Produto,
+      props: true,
     },
   ],
   // função para voltar ao topo da pagina
