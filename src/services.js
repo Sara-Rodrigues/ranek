8088bb12f777a69678a9227d16e3906b7b094490
Sara-Rodrigues <sara.slcr67@gmail.com>
2021-08-12 12:25:44 -0300

    atualizar usuario

--- src/services.js
+++ src/services.js
@@ -11,7 +11,10 @@ export const api = {
   post(endpoint, body) {
     return axiosInstance.post(endpoint, body);
   },
-  delete(enpoint) {
+  put(endpoint, body) {
+    return axiosInstance.put(endpoint, body);
+  },
+  delete(endpoint) {
     return axiosInstance.delete(endpoint);
   },
 };
